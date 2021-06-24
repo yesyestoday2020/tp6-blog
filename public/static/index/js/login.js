84cedee60c81a6a4a8b25e66ea982782922af9d1
yesyestoday2020 <heguohui2017@outlook.com>
2021-06-24 17:48:49 +0800

    第一次提交

---
+++ public/static/index/js/login.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $("button").click(function () {
+        // // 验证字段是否为空
+        // if ($('#name').val() == '') {
+        //     $('.message_name').html('名字不能为空');
+        // }
+
+        // if ($('#email').val() == '') {
+        //     $('.message_email').html('邮箱不能为空');
+        // }
+
+        // if ($('#password').val() == '') {
+        //     $('.message_password').html('密码不能为空');
+        // }
+
+        var form_data = $("#form_data").serialize();
+
+        $.ajax({
+            // 请求方法
+            type: "POST",
+            // 发送到服务器的数据
+            data: form_data,
+
+            // url :控制器/方法
+            url: "/public/index.php/index/Login/index",
+
+            // 服务器返回数据类型
+            datatype: "json",
+
+            // 请求成功运行函数
+            success: function (result) {
+                console.log(result);
+            },
+            // 请求失败运行函数
+            error: function () {
+                console.log("请求失败");
+            },
+        });
+    });
+});
