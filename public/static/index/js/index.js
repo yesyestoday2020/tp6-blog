7a45a3351adcdc6afa742ad4f78639756e7fbe55
yesyestoday2020 <heguohui2017@outlook.com>
2021-06-24 17:44:24 +0800

    第一次提交

--- public/static/index/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".opensidebar").click(function () {
-        $(".ui.sidebar").sidebar("toggle");
-    });
-
-    $(".preview").click(function () {
-        $(".ui.modal").modal("show");
-    });
-});
